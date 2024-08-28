6088e9ea2100e773fede677cb59e45adac5dd9fc
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-28 07:24:16 +0200

    Add About me component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,30 @@
+import styles from './About.module.css';
+function About() {
+  return (
+    <section className={styles.about}>
+      <h2>Kim jestem</h2>
+      <div className={styles.profession}>Full Stack Dev</div>
+      <img
+        src="../../assets/images/wojtek-02.jpeg"
+        alt="Me standing"
+        className={styles.aboutImg}
+      />
+      <p>
+        Projektowanie stron internetowych była od zawsze moim marzeniem, ale
+        długo nie robiłem nic w tym kierunku. Kurs Harvard CS50P, który opierał
+        się na Pythonie zaszczepil we mnie ogromny zapał do tego zawodu. Kolejny
+        kurs CS50X i poznanie podstaw języka C z manulanym zarząniem pamięcią i
+        pointereami zaowocowal przy głębszej juz nauce Javascript.
+      </p>
+      <p>
+        Moją przygodę z nauką web dev kontynuwalem z Odin Project, który po 7
+        miesiącach ukończyłem. CSS, mimo, że z początku sprawial sporo
+        flustracji, po poznaniu FlexBox i GRid zaczął również sprawiać
+        przyjemność. W tym momencie doskonale wiedze w kierunku React, który
+        doskonale scala wszystkie narzędzia. W miedzyczasie poznalem Node JS i
+        posgresql.
+      </p>
+    </section>
+  );
+}
+export default About;
