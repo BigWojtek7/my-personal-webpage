138cea5573a1cd1fd71e2065127b7f5e82739072
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-29 20:46:57 +0200

    Add 100vh to intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,13 +1,18 @@
 import styles from './Intro.module.css';
 function Intro() {
   return (
-    <section className={styles.intro} id='home'>
-      <h1 className={styles.title}>
-        Cześć, jestem <strong>Wojciech Zagórowski</strong>
-      </h1>
-      <p className={styles.subtitle}> full-stack dev</p>
-        <img className={styles.introImg} src="/images/wojtek-01.jpeg" alt="Image of my face" />
-
+    <section className={styles.intro} id="home">
+      <div className={styles.card}>
+        <h1 className={styles.title}>
+          Cześć, jestem <strong>Wojciech Zagórowski</strong>
+        </h1>
+        <p className={styles.subtitle}> full-stack dev</p>
+        <img
+          className={styles.introImg}
+          src="/images/wojtek-01.jpeg"
+          alt="Image of my face"
+        />
+      </div>
     </section>
   );
 }
