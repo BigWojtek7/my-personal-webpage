2368f76473d33db986af6901dee73ad27eba90d2
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-30 09:22:04 +0200

    Add projects array to display projects

---
+++ src/assets/projects/projects.js
@@ -0,0 +1,17 @@
+const projectsArray = [
+  {
+    id: 1,
+    project_name: 'The friendsBook',
+    subtitle: 'React + NodeJS + SQL',
+    image_url: '/images/portfolio-01.jpg',
+  },
+  {
+    id: 2,
+    project_name: 'Blog API',
+    subtitle: 'React + NodeJS + MongoDB',
+    image_url: '/images/portfolio-02.jpg',
+
+  }
+];
+
+export default projectsArray;
