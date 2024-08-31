c6bb051f224d16b4b1a7c6a976d1989a3283d073
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-31 16:01:25 +0200

    Add 2 project templates in my Work section

--- src/assets/projects/projects.js
+++ src/assets/projects/projects.js
@@ -43,6 +43,7 @@ const projectsArray = [
       frontend: 'https://github.com/BigWojtek7/odin-book-frontend',
       backend: 'https://github.com/BigWojtek7/odin-book-backend',
     },
+    live_url: 'https://odin-books.netlify.app/',
   },
   {
     id: 3,
@@ -65,6 +66,7 @@ const projectsArray = [
       frontend: 'https://github.com/BigWojtek7/odin-book-frontend',
       backend: 'https://github.com/BigWojtek7/odin-book-backend',
     },
+    live_url: 'https://odin-books.netlify.app/',
   },
   {
     id: 4,
@@ -87,7 +89,54 @@ const projectsArray = [
       frontend: 'https://github.com/BigWojtek7/odin-book-frontend',
       backend: 'https://github.com/BigWojtek7/odin-book-backend',
     },
+    live_url: 'https://odin-books.netlify.app/',
   },
+  {
+    id: 5,
+    name: 'The friendsBook',
+    subtitle: 'React + NodeJS + SQL',
+    image_screenshot: {
+      image_url: '/images/portfolio-05.jpg',
+      image_alt: 'Screenshot of my project',
+    },
+    image_details: {
+      image_url: '/images/portfolio-01/detail.jpg',
+      image_alt: 'Screenshot of my project',
+    },
+    description: {
+      intro: 'Projekt mini facebook w React i NodeJS',
+      body: 'Możliwość stworzenia swojego konta i dodawania znajomych. Auntentyfikacją za sprawą JWT token i passport. Obsługa bazy danych za pomocą PosGreSQL bez zadnych OMD w ramach cwiczen zapytan SQL. Po stronie klienta React i responsywny layout CSS w module CSS ',
+      ending: 'Ten projekt dużo nauczył mnie jeżeli chodzi o SQL',
+    },
+    github_url: {
+      frontend: 'https://github.com/BigWojtek7/odin-book-frontend',
+      backend: 'https://github.com/BigWojtek7/odin-book-backend',
+    },
+    live_url: 'https://odin-books.netlify.app/',
+  },  {
+    id: 6,
+    name: 'The friendsBook',
+    subtitle: 'React + NodeJS + SQL',
+    image_screenshot: {
+      image_url: '/images/portfolio-06.jpg',
+      image_alt: 'Screenshot of my project',
+    },
+    image_details: {
+      image_url: '/images/portfolio-01/detail.jpg',
+      image_alt: 'Screenshot of my project',
+    },
+    description: {
+      intro: 'Projekt mini facebook w React i NodeJS',
+      body: 'Możliwość stworzenia swojego konta i dodawania znajomych. Auntentyfikacją za sprawą JWT token i passport. Obsługa bazy danych za pomocą PosGreSQL bez zadnych OMD w ramach cwiczen zapytan SQL. Po stronie klienta React i responsywny layout CSS w module CSS ',
+      ending: 'Ten projekt dużo nauczył mnie jeżeli chodzi o SQL',
+    },
+    github_url: {
+      frontend: 'https://github.com/BigWojtek7/odin-book-frontend',
+      backend: 'https://github.com/BigWojtek7/odin-book-backend',
+    },
+    live_url: 'https://odin-books.netlify.app/',
+  },
+  
 ];
 
 export default projectsArray;
